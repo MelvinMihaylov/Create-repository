4b2f0334c0304ae7683b6fd792d23e2fd01d9762
Mihaylov <Mihaylov@DESKTOP-04HRDUL>
2021-03-18 09:30:45 +0200

    commit

--- prj1/src/prj1/User.java
+++ prj1/src/prj1/User.java
@@ -1,5 +1,15 @@
 package prj1;
 
 public class User {
-
+   private String name;
+   
+   public String getName() {
+	   return name;
+   }
+   
+  public void setname(StringName) {
+	  this.name = name;
+	  
+  }
+  
 }
--- prj1/src/prj1/clas.java
+++ prj1/src/prj1/clas.java
@@ -1,10 +1,7 @@
 package prj1;
 
 public class clas {
-	 int x = 5;
-
 	  public static void main(String[] args) {
-	    clas myObj = new clas();
-	    System.out.println(myObj.x);
+	   System.out.println("Hello World!");
 	  }
 }
